5a2cb3f722416b48b98ca325f46d2bccc80f8b44
Parth7864 <ylmcofficial@gmail.com>
2026-07-18 21:23:35 +0800

    Content file
    This file contains the injection part.

---
+++ content.js
@@ -0,0 +1,39 @@
+function injectScript() {
+
+    if (document.documentElement.dataset.inputBridgeLoaded) {
+        return;
+    }
+
+    document.documentElement.dataset.inputBridgeLoaded = "true";
+
+
+    const script = document.createElement("script");
+
+    script.src = chrome.runtime.getURL(
+        "main-world.js"
+    );
+
+
+    script.onload = () => {
+        script.remove();
+    };
+
+
+    document.documentElement.appendChild(script);
+}
+
+
+function checkPage() {
+
+    const isGamePage =
+        location.pathname.includes("/play/launch/");
+
+
+    const video =
+        document.querySelector("video");
+
+
+    if (isGamePage && video) {
+        injectScript();
+
+    }
